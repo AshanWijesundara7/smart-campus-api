90b7401fee5da6113a87c21b696b35bb81457279
AshanMalidu <ashan.20230392@iit.ac.lk>
2026-04-24 04:55:39 +0530

    add main code file

---
+++ src/main/java/com/smartcampus/Main.java
@@ -0,0 +1,35 @@
+package com.smartcampus;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class Main {
+
+    public static final String BASE_URI = "http://0.0.0.0:8080/api/v1/";
+
+    public static void main(String[] args) throws IOException {
+        ResourceConfig config = new ResourceConfig()
+                .packages(
+                    "com.smartcampus.resource",
+                    "com.smartcampus.exception",
+                    "com.smartcampus.filter"
+                );
+
+        HttpServer server = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+
+        System.out.println("==============================================");
+        System.out.println("  Smart Campus API is running!");
+        System.out.println("  Base URL : http://localhost:8080/api/v1");
+        System.out.println("  Discovery: GET http://localhost:8080/api/v1/");
+        System.out.println("  Rooms    : GET http://localhost:8080/api/v1/rooms");
+        System.out.println("  Sensors  : GET http://localhost:8080/api/v1/sensors");
+        System.out.println("==============================================");
+        System.out.println("Press ENTER to stop...");
+        System.in.read();
+        server.shutdownNow();
+    }
+}
